c5bc339a6118acc0cd27c291dfa0e7dea2e6f66c
Austin Aigbe <eshikafe@gmail.com>
2021-06-28 17:38:43 +0100

    Create db.rs

---
+++ src/db.rs
@@ -0,0 +1,19 @@
+use redis::Commands;
+
+fn get_rule_set(rule_set: &str) -> String {
+    let mut conn = redis_connect();
+    let result: String = redis::cmd("GET")
+        .arg(rule_set)
+        .query(&mut conn)
+        .expect("Failed to GET ruleset")
+    result
+}
+
+
+// Redis docker container must be running
+fn redis_connect() -> redis::Connection {
+    redis::Client::open("redis://localhost:6379")
+        .expect("Redis DB connection error")
+        .get_connection()
+        .expect("Failed to connect to redis")
+}
