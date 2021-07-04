d1b294ebadc50d93855793428520205de9dbcd0d
eshikafe <eshikafe@gmail.com>
2021-07-04 16:05:01 +0100

    Fix redis error

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 // PAWS - Protocol to Access White Space Database
 // RFC 7545
 
-pub mod parameters;
-pub mod methods;
-pub mod errors;
-pub mod types;
-pub mod version;
+// pub mod parameters;
+// pub mod methods;
+// pub mod errors;
+// pub mod types;
+// pub mod version;
 pub mod db;
 
 #[cfg(test)]
