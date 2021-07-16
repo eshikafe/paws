34f20c8b1056c5efa14ec8dec24f7e66669ec123
eshikafe <eshikafe@gmail.com>
2021-07-16 23:26:56 +0100

    update test

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,15 @@
 // pub mod errors;
 // pub mod types;
 // pub mod version;
-pub mod db;
+pub mod redis_client;
 
 #[cfg(test)]
 mod paws_tests {
-    use super::db;
+    use super::redis_client as rc;
     #[test]
     fn test_redis_db() {
-        let r1 = db::get_ruleset("nccId");
-        let r2 = db::get_ruleset("fccId");
+        let r1 = rc::get_ruleset("nccId");
+        let r2 = rc::get_ruleset("fccId");
         assert_eq!("123", r1);
         assert_eq!("12345", r2);
         println!("value for nccId = {}", r1);
