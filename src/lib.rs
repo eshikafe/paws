ea3ea5cb4a2345d638f4e545092418ec0150e581
Austin Aigbe <eshikafe@gmail.com>
2021-07-31 15:41:30 +0100

    Add test for MAC address

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,10 @@
 // PAWS - Protocol to Access White Space Database
 // Compliant with RFC 7545
 
-// pub mod parameters;
+pub mod types;
+pub mod parameters;
 // pub mod methods;
 // pub mod errors;
-// pub mod types;
 // pub mod version;
 pub mod redis_client;
 pub mod server;
@@ -14,7 +14,7 @@ pub mod server;
 #[cfg(test)]
 mod paws_tests {
     use super::redis_client as rc;
-    use mac_address::get_mac_address;
+    use super::parameters::get_mac_addr;
     
     #[test]
     #[ignore]
@@ -29,20 +29,14 @@ mod paws_tests {
     }
     
     #[test]
-    #[ignore]
-    fn get_mac_addr() {
-        match get_mac_address() {
-        Ok(Some(ma)) => {
-            println!("MAC addr = {}", ma);
-            println!("bytes = {:?}", ma.bytes());
-        }
-        Ok(None) => println!("No MAC address found."),
-        Err(e) => println!("{:?}", e),
-        }
+    fn test_mac_addr() {
+        let m = get_mac_addr();
+        println!("{}",m);
     }
 
     use super::server;
     #[test]
+    #[ignore]
     fn test_server() {
         server::start();
     }
