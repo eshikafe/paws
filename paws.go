623df11cda0314d837fa080d25947407ffbd7233
Austin Aigbe <eshikafe@gmail.com>
2021-09-07 06:13:13 +0100

    Minimal PAWS server implementation

--- paws.go
+++ paws.go
@@ -2,25 +2,23 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"paws/paws"
 )
 
 func main() {
-	fmt.Println("PAWS protocol, version " + paws.PAWSVersion)
+	// fmt.Println("PAWS protocol, version " + paws.PAWSVersion)
 
 	// INIT_MSG
-	request := paws.Request{}
-	msg := request.InitMsg()
-	req, _ := json.MarshalIndent(msg, "", "    ")
-	fmt.Println("PAWS Request:")
-	fmt.Println(string(req))
+	// var request paws.Request
+	// msg := request.InitMsg()
+	// req, _ := json.Marshal(msg)
+	// fmt.Println(string(req))
 
 	// INT_RES
-	response := paws.Response{}
-	msg2 := response.InitMsg()
-	res, _ := json.MarshalIndent(msg2, "", "    ")
-	fmt.Println("PAWS Response:")
-	fmt.Println(string(res))
+	// var response paws.Response
+	// msg2 := response.InitMsg()
+	// res, _ := json.MarshalIndent(msg2, "", "    ")
+	// fmt.Println(string(res))
+
+	paws.StartServer()
 }
