3643acdb7bd476f8d486b7d28164c6f9167fa3bd
Austin Aigbe <eshikafe@gmail.com>
2021-09-12 22:19:13 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,21 @@
 
 pub mod types;
 pub mod parameters;
-pub mod methods;
+pub mod method;
 // pub mod errors;
 pub mod version;
 pub mod redis_client;
 pub mod server;
+pub mod message;
 
 
 #[cfg(test)]
-mod paws_tests {
+mod test_cases {
     use super::redis_client as rc;
     use super::parameters::get_mac_addr;
+    use super::server;
+    use super::message;
+    use super::method;
     
     #[test]
     #[ignore]
@@ -29,14 +33,26 @@ mod paws_tests {
     }
     
     #[test]
+    #[ignore]
     fn test_mac_addr() {
         let m = get_mac_addr();
         println!("{}",m);
     }
 
-    use super::server;
+    
     #[test]
+    #[ignore]
     fn test_server() {
         server::start();
+    }
+
+    #[test]
+    fn test_message() {
+        let init_req = message::InitReq::new();
+        let req = message::Request::new(init_req);
+        let s = serde_json::to_string_pretty(&req).unwrap();
+        println!("JSON: PAWS INIT_REQ: {}", s);
+        println!("Struct: PAWS INIT_REQ: {:?}", req);
+        // println!("Struct: PAWS INIT_REQ: {:?}", init_req);
     }
 }
