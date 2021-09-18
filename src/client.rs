fdb14609b9a37b02da123ffb0923d8973da67bb8
Austin Aigbe <eshikafe@gmail.com>
2021-09-18 15:20:23 +0100

    Update client.rs

--- src/client.rs
+++ src/client.rs
@@ -1,19 +1,20 @@
-use hyper::{Body, Method, Request, Uri};
-use method;
+// use hyper::{Body, Method, Request, Uri};
+use paws::method;
 
 /// The POST method is the only method REQUIRED for PAWS.
 /// A PAWS request message is carried in the body of an HTTP POST request.
 /// A PAWS response SHOULD include a Content-Length header.
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let req = Request::builder()
-        .method(Method::POST)
-        .uri("http://localhost:3030")
-        .header("content-type", "application/json")
-        .body(Body::from(r#"{"library":"hyper"}"#))?;
+async fn main() { // -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    println!("PAWS Client");
+//     let req = Request::builder()
+//         .method(Method::POST)
+//         .uri("http://localhost:3030")
+//         .header("content-type", "application/json")
+//         .body(Body::from(r#"{"library":"hyper"}"#))?;
 
-    // We'll send it in a second...
+//     // We'll send it in a second...
 
-    Ok(())
+//     Ok(())
 }
